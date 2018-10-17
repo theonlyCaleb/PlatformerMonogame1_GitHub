d2b01f3d0eee0a350cc649e248bb1afb2634f8bb
Caleeb <c.scotton.02@gmail.com>
2018-10-17 16:11:16 +1000

    Commitment of project files
    - Created player class
    - Created sprite class
    - Coded Game class to draw sprite to screen

---
+++ PlatformerMonogame1/Player.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace PlatformerMonogame1
+{
+    class Player
+    {
+        Sprite playerSprite = new Sprite();
+
+        public Player()
+        {
+
+        }
+
+        public void Load (ContentManager content)
+        {
+            playerSprite.Load(content, "hero");
+        }
+
+        public void Update (float deltaTime)
+        {
+            playerSprite.Update(deltaTime);
+        }
+
+        public void Draw (SpriteBatch spriteBatch)
+        {
+            playerSprite.Draw(spriteBatch);
+        }
+
+
+    }
+}
